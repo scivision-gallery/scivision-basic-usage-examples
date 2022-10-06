62f24c9927a687276963346fd5bf7fd16521509f
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-10-06 10:07:11 +0100

    begin experimentation with vedge detector

--- stac.py
+++ stac.py
@@ -13,12 +13,14 @@
 #     name: python3
 # ---
 
-from scivision import load_dataset
+from scivision import load_dataset, default_catalog, load_pretrained_model
 import matplotlib.pyplot as plt
 
 data = load_dataset('https://github.com/alan-turing-institute/scivision_sentinel2_stac')
 
 yy = data.load_data(resolution=20, bands=("red", "green", "blue"))
+
+type(yy)
 
 yy = yy.compute()
 
@@ -34,3 +36,19 @@ _ = (
 )
 
 plt.show()
+
+data_catalog = default_catalog.datasources.to_dataframe()
+data_catalog
+
+dataset_name = data_catalog.loc[data_catalog['url'] == 'https://github.com/alan-turing-institute/scivision_sentinel2_stac'].name.values[0]
+
+compatible_models = default_catalog.compatible_models(dataset_name).to_dataframe()
+compatible_models
+
+model = load_pretrained_model('https://github.com/MartinSJRogers/VEdge_Detector_scivision', allow_install=True)
+model
+
+# Notes:
+#
+# - To run vedgedetector requires python 3.7 becuase of tensorflow
+# - odc-stac used by the data plugin requires 3.8+
